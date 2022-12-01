27c53b489e4fd936797feb50124b9507dd0be28e
= <rickard_hallerback@hotmail.com>
2022-12-01 07:01:41 +0100

    dag 1

--- 1.py
+++ 1.py
@@ -2,5 +2,21 @@
 
 from general import *
 
-print(readlinesAsIntsFromFile("inputs/test1.txt"))
+lines = readlinesFromFile("inputs/1.txt")
+max_ = 0
+count = 0
 
+counts = [0]
+count_i = 0
+
+for line in lines:
+    if len(line) == 1:
+        counts.append(0)
+        count = 0
+        count_i += 1
+    else:
+        counts[count_i] += int(line)
+
+print(counts)
+counts.sort(reverse=True)
+print(counts[0]+counts[1]+counts[2])
